36349baf79604b2b92fe39b787a51af312e962a7
Anastasiia Latysh <anastasiialatysh@anastasiias-macbook-pro.local>
2019-04-13 19:47:52 +0300

    Initial Commit

---
+++ KP1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  KP1
+//
+//  Created by Anastasiia Latysh on 4/13/19.
+//  Copyright © 2019 Anastasiia Latysh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
